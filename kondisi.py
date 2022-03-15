8d4665c125008d4b990dad6889eead621d5b30f6
Han <101443278+Handra1@users.noreply.github.com>
2022-03-15 14:05:06 +0700

    Add files via upload

---
+++ kondisi.py
@@ -0,0 +1,17 @@
+z = 4
+if z % 2 == 0 :
+    print("z is even")
+
+z = 5
+if z % 2 == 0 :
+    print("z is even")
+else:
+    print("z is odd")
+
+z = 3
+if z % 2 == 0 :
+    print("z is divisible by 2")
+elif z % 3 == 0 :
+    print("z is divisible by 3")
+else:
+    print("z neither divisible by 2 nor by 3")
